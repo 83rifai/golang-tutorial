07425b55f031456e041869d8d86ace78b7f2f4b9
83rifai <83rifai@gmail.com>
2017-07-27 11:53:09 +0700

    varieable

---
+++ varieable/varieable.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+
+func main() {
+  var a string = "GOLANG VARIEABLES"
+  fmt.Println(a) // print varieable
+
+  var intA, intB int = 1,2
+  fmt.Println(intA,intB)
+}
