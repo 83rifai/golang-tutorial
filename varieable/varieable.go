0fdd7ad6a917eeb45d09ee6082c50059e6892508
83rifai <83rifai@gmail.com>
2017-07-27 12:16:35 +0700

    update

--- varieable/varieable.go
+++ varieable/varieable.go
@@ -8,5 +8,14 @@ func main() {
   fmt.Println(a) // print varieable
 
   var intA, intB int = 1,2
-  fmt.Println(intA,intB)
+  fmt.Println(intA,intB) // print int data varieable
+
+  var b = true
+  fmt.Println(b)
+
+  var c int
+  fmt.Println(c)
+
+  d := "string"
+  fmt.Println(d)
 }
