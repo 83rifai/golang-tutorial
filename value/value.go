72b5565f9d05e06dbf1d6148c4e441a5a9e83175
83rifai <83rifai@gmail.com>
2017-07-27 11:40:04 +0700

    commit

---
+++ value/value.go
@@ -0,0 +1,16 @@
+// This Example for value
+
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("GOLANG "+" VALUE") // this value string
+
+  fmt.Println("1 + 1 = ", 1+1) // this value number
+
+  fmt.Println("7.0 / 3.0", 7.0/3.0) // this decimal
+
+  fmt.Println(true && true)
+
+}
